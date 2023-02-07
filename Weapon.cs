966071a5ec0edae4f75c8de459bde81a34d11b61
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-07 15:18:01 +0100

    feat: A weapon can now be created
    Closes #5

---
+++ Weapon.cs
@@ -0,0 +1,44 @@
+﻿using RPGHeroes.Abstract;
+using RPGHeroes.Enums;
+using RPGHeroes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes
+{
+    internal class Weapon : IEquippable
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            private set => _name = value;
+        }
+        private int _requiredValue;
+        public int RequiredLevel
+        {
+            get => _requiredValue;
+            private set => _requiredValue = value;
+        }
+        private EquipmentSlot _equipmentSlot;
+        public EquipmentSlot Slot
+        {
+            get => _equipmentSlot;
+            private set => _equipmentSlot = value;
+        }
+        public int WeaponDamage { get; private set; }
+        public WeaponType WeaponType { get; private set; }
+
+        public Weapon(string name, int requiredLevel, int weaponDamage, WeaponType weaponType)
+        {
+            _name = name;
+            _requiredValue = requiredLevel;
+            _equipmentSlot = EquipmentSlot.Weapon;
+            WeaponDamage = weaponDamage;
+            WeaponType = weaponType;
+        }
+    }
+}
