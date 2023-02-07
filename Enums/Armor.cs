7421cae941bdfd068d5c5fcc9686b1b14c36a7ec
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-07 15:38:19 +0100

    feat: Adds armor equippable
    Closes #6

---
+++ Enums/Armor.cs
@@ -0,0 +1,43 @@
+﻿using RPGHeroes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Enums
+{
+    internal class Armor : IEquippable
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            private set => _name = value;
+        }
+        private int _requiredValue;
+        public int RequiredLevel
+        {
+            get => _requiredValue;
+            private set => _requiredValue = value;
+        }
+        private EquipmentSlot _equipmentSlot;
+        public EquipmentSlot Slot
+        {
+            get => _equipmentSlot;
+            private set => _equipmentSlot = value;
+        }
+        public HeroAttribute ArmorAttribute { get; private set; }
+        public ArmorType ArmorType { get; private set; }
+
+        public Armor(string name, int requiredLevel, HeroAttribute armorAttribute, EquipmentSlot equipmentSlot, ArmorType armorType)
+        {
+            _name = name;
+            _requiredValue = requiredLevel;
+            _equipmentSlot = equipmentSlot;
+            ArmorAttribute = armorAttribute;
+            ArmorType = armorType;
+        }
+
+    }
+}
---
+++ Interfaces/HeroAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Interfaces
+{
+    public struct HeroAttribute
+    {
+        public int strength;
+        public int dexterity;
+        public int intelligence;
+
+        public HeroAttribute(int strength, int dexterity, int intelligence)
+        {
+            this.strength = strength;
+            this.dexterity = dexterity;
+            this.intelligence = intelligence;
+        }
+
+        public HeroAttribute Increase(HeroAttribute other)
+        {
+            return new HeroAttribute(this.strength + other.strength, this.dexterity + other.dexterity, this.intelligence + other.intelligence);
+        }
+
+        public HeroAttribute Increase(int strength, int dexterity, int intelligence)
+        {
+            return new HeroAttribute(this.strength + strength, this.dexterity + dexterity, this.intelligence + intelligence);
+        }
+    }
+}
