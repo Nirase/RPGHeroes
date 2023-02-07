7f5a56bccb67ba7626aa6dc53d0f2b957e23903d
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-07 16:10:36 +0100

    feat: Adds a class interface and implements all classes
    Closes #4

---
+++ Classes/Mage.cs
@@ -0,0 +1,24 @@
+﻿using RPGHeroes.Enums;
+using RPGHeroes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Classes
+{
+    public class Mage : IClass
+    {
+        public ClassType ClassType => ClassType.Mage;
+        public HeroAttribute BaseStats => new(1, 1, 8);
+        public HeroAttribute LevelUpStats => new(1, 1, 5);
+        public WeaponType[] AllowedWeapons => new WeaponType[] { WeaponType.Staff, WeaponType.Wand };
+        public ArmorType[] AllowedArmor => new ArmorType[] { ArmorType.Cloth };
+
+        public HeroAttribute LevelUp()
+        {
+            return LevelUpStats;
+        }
+    }
+}
