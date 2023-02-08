5a67c22f21b69befd7b57bbf6b58f09fe19e3d13
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-08 16:06:20 +0100

    feat: add copy constructor and operator overloading
    WIP #11

--- Interfaces/HeroAttribute.cs
+++ Interfaces/HeroAttribute.cs
@@ -12,6 +12,12 @@ namespace RPGHeroes.Interfaces
         public int dexterity;
         public int intelligence;
 
+        public HeroAttribute(HeroAttribute other)
+        {
+            strength = other.strength;
+            dexterity= other.dexterity;
+            intelligence = other.intelligence;
+        }
         public HeroAttribute(int strength, int dexterity, int intelligence)
         {
             this.strength = strength;
@@ -27,6 +33,11 @@ namespace RPGHeroes.Interfaces
         public HeroAttribute Increase(int strength, int dexterity, int intelligence)
         {
             return new HeroAttribute(this.strength + strength, this.dexterity + dexterity, this.intelligence + intelligence);
+        }
+
+        public static HeroAttribute operator +(HeroAttribute a, HeroAttribute b)
+        {
+            return new HeroAttribute(a.strength + b.strength, a.dexterity + b.dexterity, a.intelligence + b.intelligence);
         }
     }
 }
