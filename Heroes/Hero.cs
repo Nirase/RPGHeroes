948723bb36757422e727216528131a673c9eda0c
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-08 16:07:18 +0100

    feat: Make hero a single class
    Removes all specific IClass implementations in favour of using an enum to decide the stats for the hero
    WIP #11

---
+++ Heroes/Hero.cs
@@ -0,0 +1,106 @@
+﻿using RPGHeroes.Enums;
+using RPGHeroes.Equipment;
+using RPGHeroes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Heroes
+{
+    public class Hero : IClass
+    {
+        public string Name { get; set; }
+        private HeroAttribute _stats;
+        public int Level { get; private set; }
+
+        private ClassType _classType;
+        public ClassType ClassType { get => _classType; }
+
+        private HeroAttribute _baseStats;
+        public HeroAttribute BaseStats { get => _baseStats; }
+        
+        private HeroAttribute _levelUpStats;
+        public HeroAttribute LevelUpStats { get => _levelUpStats; }
+        
+        public WeaponType[] _allowedWeapons;
+        public WeaponType[] AllowedWeapons { get => _allowedWeapons; }
+        
+        private ArmorType[] _allowedArmor;
+        public ArmorType[] AllowedArmor { get => _allowedArmor; }
+        Dictionary<EquipmentSlot, IEquippable> _equipmentSlots;
+
+        public Hero(string name, ClassType classType)
+        {
+            _equipmentSlots = new();
+            this.Name = name;
+            this._classType = classType;
+            
+            switch(classType) 
+            {
+                case ClassType.Mage:
+                    _allowedWeapons = new WeaponType[] { WeaponType.Staff, WeaponType.Wand };
+                    _allowedArmor = new ArmorType[] { ArmorType.Cloth };
+                    _baseStats = new(1, 1, 8);
+                    _levelUpStats = new(1, 1, 5);
+                    break;
+                case ClassType.Warrior:
+                    _allowedWeapons = new WeaponType[] { WeaponType.Axe, WeaponType.Sword, WeaponType.Hammer };
+                    _allowedArmor = new ArmorType[] { ArmorType.Mail, ArmorType.Plate };
+                    _baseStats = new(5, 2, 1);
+                    _levelUpStats = new(3, 2, 1);
+                    break;
+                case ClassType.Ranger:
+                    _allowedWeapons = new WeaponType[] { WeaponType.Bow };
+                    _allowedArmor = new ArmorType[] { ArmorType.Leather, ArmorType.Mail };
+                    _baseStats = new(1, 7, 1);
+                    _levelUpStats = new(1, 5, 1);
+                    break;
+                case ClassType.Rogue:
+                    _allowedWeapons = new WeaponType[] { WeaponType.Dagger, WeaponType.Sword };
+                    _allowedArmor = new ArmorType[] { ArmorType.Leather, ArmorType.Mail };
+                    _baseStats = new(2, 6, 1);
+                    _levelUpStats = new(1, 4, 1);
+                    break;
+                default:
+                    _allowedArmor = new ArmorType[0];
+                    _allowedWeapons = new WeaponType[0];
+                    _baseStats = new(0, 0, 0);
+                    _levelUpStats = new(0, 0, 0);
+                    break;
+            }
+        }
+
+
+        public void Equip(IEquippable equippable)
+        {
+
+            try
+            {
+                if (equippable.Equip(this))
+                    _equipmentSlots[equippable.Slot] = equippable;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        HeroAttribute TotalAttributes()
+        {
+            HeroAttribute result = new HeroAttribute(_stats);
+            foreach(Armor value in _equipmentSlots.Values)
+            {
+                result += value.ArmorAttribute;
+            }
+
+            return result;
+        }
+
+        void IClass.LevelUp()
+        {
+            ++Level;
+            _stats += _levelUpStats;
+        }
+    }
+}
