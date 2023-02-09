431e66a30ccbe7f8ec25a7478459ffe44b7d17fc
MSmed <mattias.smedman@gmail.com>
2023-02-09 16:38:43 +0100

    feat: Tests
    Actually adds all tests after they were put in the wrong folder by mistake.
    WIP #13

---
+++ RPGHeroesTests/HeroAttribute/HeroAttributeTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPGHeroes.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes
+{
+    [TestClass]
+    public class HeroAttributeTests
+    {
+        [TestMethod]
+        public void EqualsTest()
+        {
+            HeroAttribute attribute = new(1, 2, 3);
+            HeroAttribute secondAttribute = new(1, 2, 3);
+            Assert.IsTrue(attribute == secondAttribute);
+        }
+        [TestMethod]
+        public void NotEqualsTest()
+        {
+            HeroAttribute attribute = new(1, 2, 3);
+            HeroAttribute secondAttribute = new(2, 3, 4);
+            Assert.IsTrue(attribute != secondAttribute);
+        }
+    }
+}
